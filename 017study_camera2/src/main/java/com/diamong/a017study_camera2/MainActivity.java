f7bbc43d7392659c65b34e8cb9627ff0863fee1c
diamong01 <45653307+diamong01@users.noreply.github.com>
2019-05-15 01:09:34 +0900

    017 study - camera2
    런타임 리퀘스트 퍼미션 다중 요청완료, 카메라 저장, 불러오기 완료

--- 017study_camera2/src/main/java/com/diamong/a017study_camera2/MainActivity.java
+++ 017study_camera2/src/main/java/com/diamong/a017study_camera2/MainActivity.java
@@ -3,18 +3,29 @@ package com.diamong.a017study_camera2;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 
 public class MainActivity extends AppCompatActivity {
+    private String currentPhotoPath;
 
 
     @Override
@@ -54,11 +65,77 @@ public class MainActivity extends AppCompatActivity {
 
         if (camera && write){
             //사진찍는 인텐트 추가
-             Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-             startActivityForResult(intent,0);
+            TakePicture();
 
-        }else {
+        } else {
             Toast.makeText(this, "사진찍고 저장하는 권한이 없습니다.", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    private void TakePicture() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        try {
+            File photoFile = createImageFile();
+            Uri photoUri = FileProvider.getUriForFile(this, "com.diamong.a017study_camera2", photoFile);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+            startActivityForResult(intent, 0);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        //File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == 0 && resultCode == RESULT_OK) {
+            ImageView imageView = findViewById(R.id.img_thumbnail);
+
+            imageView.setImageBitmap(BitmapFactory.decodeFile(currentPhotoPath));
+        }
+        if (requestCode == 1001 && resultCode == RESULT_OK) {
+            Uri uri = data.getData();
+            ImageView imageView2 = findViewById(R.id.img_gallery);
+
+            imageView2.setImageURI(uri);
+
+        }
+
+    }
+
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(currentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
+        Toast.makeText(this, "저장되었습니다.", Toast.LENGTH_SHORT).show();
+    }
+
+    public void PictureSave(View view) {
+        galleryAddPic();
+    }
+
+    public void GalleryPic(View view) {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+        intent.setType("image/*");
+        startActivityForResult(intent, 1001);
     }
 }
